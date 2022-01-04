5a7455f0f4d5c6c22f371c3959793d70b146b588
kejlor <woj450@gmail.com>
2022-01-04 18:39:00 +0100

    Added edit option to the UpdateExpenseView and updating TextFields with value from entity.

--- ExpenseTracker/Views/UpdateExpenseView.swift
+++ ExpenseTracker/Views/UpdateExpenseView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct UpdateExpenseView: View {
     let expense: ExpenseEntity
     @EnvironmentObject var vm: CoreDataViewModel
+    @Binding var isEditing: Bool
     @State private var titleTextField = ""
     @State private var moneyTextField = 0.0
     @State private var selectedDate = Date()
@@ -19,21 +20,33 @@ struct UpdateExpenseView: View {
         VStack {
             Text("Expense title:")
             TextField("Enter your expense title here...", text: $titleTextField)
+                .onAppear {
+                    titleTextField = expense.title ?? "unknown"
+                }
             Text("Money spent:")
             TextField("Type book pages count here...", value: $moneyTextField, formatter: NumberFormatter())
-                .keyboardType(.numberPad)
+                .keyboardType(.numberPad).onAppear {
+                    moneyTextField = expense.money
+                }
             Text("Date of purchase:")
-            DatePicker("", selection: $selectedDate)
+            DatePicker("", selection: $selectedDate).onAppear {
+                selectedDate = expense.date ?? Date()
+            }
             Text("Category:")
             Picker("Pick category of your purchase", selection: $selectedCategory) {
                 ForEach(vm.categories, id: \.self) {
                     Text($0.title ?? "unknown")
                 }
             }
+            .onAppear {
+                selectedCategory = expense.category ?? CategoryEntity()
+            }
             Button {
                 vm.updateExpense(entity: expense, title: titleTextField, money: moneyTextField, date: selectedDate, category: selectedCategory)
+                vm.getExpenses()
+                isEditing.toggle()
             } label: {
-                Text("Add new expense")
+                Text("Update expense")
             }
         }
     }
@@ -42,6 +55,6 @@ struct UpdateExpenseView: View {
 struct UpdateExpenseView_Previews: PreviewProvider {
     static var expense = ExpenseEntity()
     static var previews: some View {
-        UpdateExpenseView(expense: expense)
+        UpdateExpenseView(expense: expense, isEditing: .constant(true))
     }
 }
